f7bbc67803ac2f69a7b942872447e19bedb07f9f
Roland Gritzer <gritzer.roland@gmail.com>
2020-10-04 23:53:47 +0200

    sz,..

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/rolandg/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/rolandg/keymap.c
@@ -24,20 +24,22 @@
  *	K400, K401, K402,       K403,       K405,       K407,       K409, K410,       K411, K413, K414  \
  */
 
+// TODO: find a solution to enable whole AltGr layer of us-intl layout. currently only those work that are not on layer 1
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all( /* Base */
 	KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,  KC_HOME,
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC, KC_END,
 	TT(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
 	KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
-	KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           MO(1),            KC_SPC,           KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           MO(1),            KC_SPC,           TT(1),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_all( /* FN */
 	KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_PSCR,
 	RESET,    _______, _______, KC_VOLD, KC_VOLU, _______, _______, US_UDIA, _______, US_ODIA,  _______, _______, _______,          _______, _______,
-	_______,  US_ADIA, KC_MNXT, KC_MPLY, KC_MUTE, _______, _______, _______, _______, _______,  _______, _______,          _______,          _______,
+	_______,  US_ADIA,   US_SS, KC_MPLY, KC_MUTE, _______, _______, _______, _______, _______,  _______, _______,          _______,          _______,
 	_______,  _______, _______, _______, _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, _______, _______, KC_CAPS,          KC_PGUP, _______,
-	_______,  _______, _______,          _______,          _______,          _______,           _______, _______,          KC_HOME, KC_PGDN, KC_END
+	_______,  _______, _______,          _______,          _______,          _______,           _______, KC_RALT,          KC_HOME, KC_PGDN, KC_END
   /* ), */
   /* [2] = LAYOUT_all( /\* FN *\/ */
   /*   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, */
