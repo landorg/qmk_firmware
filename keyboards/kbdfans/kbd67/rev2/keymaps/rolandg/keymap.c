b7a12d65445fb47bbe1605e5a11a3178c26a2e5a
Roland Gritzer <gritzer.roland@gmail.com>
2021-01-02 00:41:20 +0100

    [rev2]: lights out, ..

--- keyboards/kbdfans/kbd67/rev2/keymaps/rolandg/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/rolandg/keymap.c
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
 #include "keymap_us_international.h"
 
@@ -25,42 +26,42 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap (Base Layer) Default Layer
 * ,----------------------------------------------------------------.
-* |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
+* |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| \|Del |Home|
 * |----------------------------------------------------------------|
-* |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
+* |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|PgUp|
 * |----------------------------------------------------------------|
-* |Caps   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
+* | Fn    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
 * |----------------------------------------------------------------|
 * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
 * |----------------------------------------------------------------|
-* |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
+* |Ctrl| Fn | Alt | Space | Win | Space  | Fn |Ctrl|  |Lef|Dow|Rig |
 * `----------------------------------------------------------------'
 */
 [0] = LAYOUT_all(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,  KC_HOME,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC, KC_END,
     TT(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
-    KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
-    KC_LCTL, MO(1),   KC_LALT,          KC_SPC,           KC_LGUI,          KC_SPC,           TT(1),   KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT),
+    KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+    KC_LCTL, MO(1),   KC_LALT,          KC_SPC,           KC_LGUI,          KC_SPC,           MO(1),   KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
 /* Keymap Fn Layer
 * ,----------------------------------------------------------------.
-* |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+* |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| \|Del |Prt |
 * |----------------------------------------------------------------|
-* |     |   |Up |   |   |   |   |   |PSc|SLk|Pau|Up |   |     |    |
+* |Reset|   |   | V-| V+|   |   |   |   | ü |   | ö |   |     |    |
 * |----------------------------------------------------------------|
-* |      |Lef|Dow|Rig|   |   |   |   |Hom|PUp|Lef|Rig|        |    |
+* |      | ä | ß |Ply|Mte|   |   |   |Lef|Dow| Up|Rig|        |    |
 * |----------------------------------------------------------------|
-* |        |   |   |   |   |   |   |   |End|PDn|Dow|      |PUp|    |
+* |        |RGB|   |   |   |   |   |   |   |   |   | Caps |PUp|    |
 * |----------------------------------------------------------------|
-* |    |    |    |                       |   |   |    |Hom|PDn|End |
+* |    |    |     |       |     |        |    |AltG|  |Hom|PDw|End |
 * `----------------------------------------------------------------'
 */
 [1] = LAYOUT_all(
-    KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_PSCR,
+    KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10, KC_F11,  KC_F12,  _______, _______, KC_PSCR,
     RESET,    _______, _______, KC_VOLD, KC_VOLU, _______, _______, US_UDIA, _______, US_ODIA,  _______, _______, _______,          _______, _______,
-    _______,  US_ADIA,   US_SS, KC_MPLY, KC_MUTE, _______, _______, _______, _______, _______,  _______, _______,          _______,          _______,
-    _______,  _______, _______, _______, _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, _______, _______, KC_CAPS,          KC_PGUP, _______,
+    _______,  US_ADIA,   US_SS, KC_MPLY, KC_MUTE, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,          _______,          _______,
+    _______,  _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_CAPS,          KC_PGUP, _______,
     _______,  _______, _______,          _______,          _______,          _______,           _______, KC_RALT, _______, KC_HOME, KC_PGDN, KC_END)
 };
 
