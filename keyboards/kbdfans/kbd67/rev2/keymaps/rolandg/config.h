2b99db19c8dbe847fa86b8068ad28a4317b0b35e
Roland Gritzer <gritzer.roland@gmail.com>
2021-01-02 00:41:20 +0100

    [rev2]: lights out, ..

--- keyboards/kbdfans/kbd67/rev2/keymaps/rolandg/config.h
+++ keyboards/kbdfans/kbd67/rev2/keymaps/rolandg/config.h
@@ -1,1 +1,9 @@
 #include "config_common.h"
+
+#define RGBLIGHT_SLEEP
+
+#undef RGBLIGHT_ANIMATIONS
+/* #define RGBLIGHT_EFFECT_BREATHING */
+
+/* #define RGBLIGHT_LAYERS */
+/* #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF */
