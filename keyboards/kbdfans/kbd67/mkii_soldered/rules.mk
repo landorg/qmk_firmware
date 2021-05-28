dced90a8f50d259389b311b6cac833ec5c95df0c
Roland Gritzer <gritzer.roland@gmail.com>
2021-05-28 02:47:04 +0200

    65_ansi_blocker_split_space

--- keyboards/kbdfans/kbd67/mkii_soldered/rules.mk
+++ keyboards/kbdfans/kbd67/mkii_soldered/rules.mk
@@ -31,5 +31,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-LAYOUTS = 65_ansi_blocker 65_ansi_blocker_split_bs 65_iso_blocker 65_iso_blocker_split_bs 65_ansi_blocker_split_bs_split_space
-
+LAYOUTS = 65_ansi_blocker 65_ansi_blocker_split_bs 65_iso_blocker 65_iso_blocker_split_bs 65_ansi_blocker_split_space 65_ansi_blocker_split_bs_split_space
