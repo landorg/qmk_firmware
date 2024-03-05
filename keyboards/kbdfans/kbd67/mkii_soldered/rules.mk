70d8e2ddd4b63a690e2a9de01b19cfb1e91d130e
Roland Gritzer <gritzer.roland@gmail.com>
2024-03-05 18:46:04 +0100

    65_ansi_blocker_split_space

--- keyboards/kbdfans/kbd67/mkii_soldered/rules.mk
+++ keyboards/kbdfans/kbd67/mkii_soldered/rules.mk
@@ -9,15 +9,10 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-<<<<<<< HEAD
-AUDIO_ENABLE = no           # Audio output
-=======
 MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-LAYOUTS = 65_ansi_blocker 65_ansi_blocker_split_bs 65_iso_blocker 65_iso_blocker_split_bs 65_ansi_blocker_split_bs_split_space
-
->>>>>>> d78de97a1c (common layout working :))
+LAYOUTS = 65_ansi_blocker 65_ansi_blocker_split_bs 65_iso_blocker 65_iso_blocker_split_bs 65_ansi_blocker_split_space 65_ansi_blocker_split_bs_split_space
---
+++ layouts/community/65_ansi_blocker_split_space/landor/readme.md
@@ -0,0 +1,11 @@
+# roland's 65% ANSI blocker space bar layout
+
+This is a 65% keyboard layout, with a blocker to the left of the arrow keys, 
+an HHKB-style (split) backspace and split space bar (2.25U 1.25U 2.75U).
+
+## Default layer
+
+![Default layer layout](https://imgur.com/a/gPbxbTV)
+
+([KLE](http://www.keyboard-layout-editor.com/#/gists/068b6617f94e2a93df6fc1c2e650f39e))
+
