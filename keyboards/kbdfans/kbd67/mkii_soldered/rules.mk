fb190324189249be379492d316a12e0d2d77f082
Roland Gritzer <gritzer.roland@gmail.com>
2024-04-20 21:47:49 +0200

    restore merge errors

--- keyboards/kbdfans/kbd67/mkii_soldered/rules.mk
+++ keyboards/kbdfans/kbd67/mkii_soldered/rules.mk
@@ -1,15 +1,12 @@
 # Build Options
 #   change yes to no to disable
 #
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-
+AUDIO_ENABLE = no           # Audio output
--- keyboards/kbdfans/kbd67/rev2/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/rules.mk
@@ -9,10 +9,4 @@ COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-
-LAYOUTS = 65_ansi 65_iso 65_ansi_blocker 65_ansi_blocker_split_bs 65_ansi_blocker_split_space 65_ansi_blocker_split_bs_split_space
+AUDIO_ENABLE = no           # Audio output
