f8919facb97fead0afbe2f29ccef8423f4293d17
Roland Gritzer <gritzer.roland@gmail.com>
2024-10-15 17:00:40 +0200

    ralt homerow mod

--- layouts/default/65_ansi_blocker_split_space/landor/keymap.c
+++ layouts/default/65_ansi_blocker_split_space/landor/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
   [0] = LAYOUT_65_ansi_blocker_split_space(
-      QK_GESC,  KC_1,               KC_2,               KC_3,               KC_4,                KC_5,    KC_6,    KC_7,               KC_8,               KC_9,               KC_0,                  KC_MINS,  KC_EQL,   KC_BSPC,  KC_HOME,
-      KC_TAB,   KC_Q,               KC_W,               KC_E,               KC_R,                KC_T,    KC_Y,    KC_U,               KC_I,               KC_O,               KC_P,                  KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END,
-      TT(2),    MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F),  KC_G,    KC_H,    MT(MOD_RSFT, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,            KC_ENT,   KC_PGUP,
-      KC_LSFT,  KC_Z,               KC_X,               KC_C,               KC_V,                KC_B,    KC_N,    KC_M,               KC_COMM,            KC_DOT,             KC_SLSH,                         KC_RSFT,  KC_UP,    KC_PGDN,
-      KC_LCTL,  MO(2),              KC_LALT,            KC_SPC,             KC_LGUI,             KC_BSPC,                                                  MO(2),              RCTL_T(KC_RALT),                 KC_LEFT,  KC_DOWN,  KC_RGHT
+      QK_GESC,  KC_1,               KC_2,               KC_3,               KC_4,                KC_5,    KC_6,    KC_7,               KC_8,               KC_9,                 KC_0,                  KC_MINS,  KC_EQL,   KC_BSPC,  KC_HOME,
+      KC_TAB,   KC_Q,               KC_W,               KC_E,               KC_R,                KC_T,    KC_Y,    KC_U,               KC_I,               KC_O,                 KC_P,                  KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END,
+      TT(2),    MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F),  KC_G,    KC_H,    MT(MOD_RSFT, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_LALT, KC_L),   MT(MOD_RGUI, KC_SCLN), KC_QUOT,            KC_ENT,   KC_PGUP,
+      KC_LSFT,  KC_Z,               MT(MOD_RALT, KC_X), KC_C,               KC_V,                KC_B,    KC_N,    KC_M,               KC_COMM,            MT(MOD_RALT, KC_DOT), KC_SLSH,                         KC_RSFT,  KC_UP,    KC_PGDN,
+      KC_LCTL,  MO(2),              KC_LALT,            KC_SPC,             KC_LGUI,             KC_BSPC,                                                  MO(2),                RCTL_T(KC_RALT),                 KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   /* Keymap (Gaming Layer)
